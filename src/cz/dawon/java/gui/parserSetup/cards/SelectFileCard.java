3fc9d7c169838bd6266dc64ce32df4dbb6591ab6
Jakub Zacek <dawon.cz@gmail.com>
2014-06-12 17:07:58 +0200

    * added components and functionality into SelectFileCard.java

--- src/cz/dawon/java/gui/parserSetup/cards/SelectFileCard.java
+++ src/cz/dawon/java/gui/parserSetup/cards/SelectFileCard.java
@@ -1,17 +1,80 @@
 package cz.dawon.java.gui.parserSetup.cards;
 
 import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 /**
  * Allows user to select single file to be parsed
  * @author Jakub Zacek
- * @version 1.0
+ * @version 1.4
  */
 public class SelectFileCard extends JPanel implements ICard {
 
 	private static final long serialVersionUID = -840790898114112531L;
+
+	private JTextField pathTF;
+	
+	/**
+	 * constructor
+	 */
+	public SelectFileCard() {
+		createMainPanel();
+	}
+	
+	
+	/**
+	 * creates main panel
+	 */
+	private void createMainPanel() {
+		this.setLayout(new FlowLayout(FlowLayout.LEFT));
+		
+		JPanel innerPN = new JPanel();
+		innerPN.setLayout(new BoxLayout(innerPN, BoxLayout.LINE_AXIS));
+		
+		innerPN.add(new JLabel("Path: "));
+		
+		pathTF = new JTextField("");
+		pathTF.setPreferredSize(new Dimension(400, 22));
+		innerPN.add(pathTF);
+	
+		this.add(innerPN);
+		
+		JButton browseBTN = new JButton("Browse...");
+		this.add(browseBTN);
+		
+		browseBTN.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				browse();
+			}
+		});
+				
+	}
+	
+	/**
+	 * when Browse button is pressed
+	 */
+	private void browse() {
+		JFileChooser fc = new JFileChooser();
+		fc.setFileSelectionMode(JFileChooser.FILES_ONLY);
+		fc.setMultiSelectionEnabled(false);
+		int res = fc.showOpenDialog(this);
+		if (res == JFileChooser.APPROVE_OPTION) {
+			pathTF.setText(fc.getSelectedFile().getAbsolutePath());
+		}
+	}
 
 	@Override
 	public int getCardId() {
@@ -67,6 +130,16 @@ public class SelectFileCard extends JPanel implements ICard {
 
 	@Override
 	public boolean onNextPress() {
+		String path = pathTF.getText();
+		if (path.trim().isEmpty()) {
+			JOptionPane.showMessageDialog(this, "You must select file to be parsed!", "Error", JOptionPane.ERROR_MESSAGE);
+			return false;
+		}
+		File f = new File(path);
+		if (!f.exists() || f.isDirectory()) {
+			JOptionPane.showMessageDialog(this, "Selected file does not exists or is a folder!", "Error", JOptionPane.ERROR_MESSAGE);
+			return false;			
+		}
 		return true;
 	}
 
